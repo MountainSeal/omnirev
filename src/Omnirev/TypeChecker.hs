e1942fb86c90f44996bb947372f9bd4cf70e54cf
MountainSeal <yamazarashi555@gmail.com>
2019-02-03 05:36:21 +0900

    [test] テストケースを変更
    - 型に関してのテストを変更・追加
    - 関数に関してのテストを変更・追加

--- src/Omnirev/TypeChecker.hs
+++ src/Omnirev/TypeChecker.hs
@@ -20,7 +20,7 @@ failure x = Bad $ "type check error: " ++ show x
 
 
 update :: String -> a -> Context a -> Context a
-update str d cxt = case (Map.lookup str cxt) of
+update str d cxt = case Map.lookup str cxt of
   Just _ -> cxt
   Nothing -> Map.insert str d cxt
 
@@ -45,10 +45,10 @@ checkDefs defs (tcxt, fcxt) = case defs of
 
 checkDef :: Def -> (Context Type, Context Iso) -> (Context Type, Context Iso, Result)
 checkDef def (tcxt, fcxt) = case def of
-  DType (Ident str) ty -> case (checkType ty tcxt) of
+  DType (Ident str) ty -> case checkType ty tcxt of
     Ok o -> (update str ty tcxt, fcxt, Ok o)
     Bad x -> (tcxt, fcxt, Bad x)
-  DFunc (Ident str) tyIn tyOut f -> case (checkFunc f (tyIn, tyOut) (tcxt, fcxt)) of
+  DFunc (Ident str) tyIn tyOut f -> case checkFunc f (tyIn, tyOut) (tcxt, fcxt) of
     Ok o -> (tcxt, update str (f, tyIn, tyOut) fcxt, Ok o)
     Bad x -> (tcxt, fcxt, Bad x)
 
@@ -76,7 +76,7 @@ checkType ty tcxt = case ty of
       res2 = checkType t2 tcxt
   TStar t -> checkType t tcxt
   TVar (Ident str) ->
-    case (Map.lookup str tcxt) of
+    case Map.lookup str tcxt of
       Just _ -> Ok ""
       Nothing -> Bad $ "not found type variable " ++ str ++ ".\n"
 
@@ -86,13 +86,13 @@ checkFunc func (tyIn, tyOut) (tcxt, fcxt) = case func of
   FId -> if tyIn == tyOut
          then Ok ""
          else Bad "id function must have same domain and codomain.\n"
-  
+
   FComp f1 f2 -> case maybeTy of
     Just _ -> Ok ""
     Nothing -> Bad "composite function must have same codomain of left side function and domain of right side function.\n"
     where
       maybeTy = searchOutType (FComp f1 f2) tyIn fcxt
-  
+
   FTensor func1 func2 -> case (tyIn, tyOut) of
     (TTensor t1 t2, TTensor t1' t2') ->
       case (res1, res2) of
@@ -104,27 +104,27 @@ checkFunc func (tyIn, tyOut) (tcxt, fcxt) = case func of
         res1 = checkFunc func1 (t1, t1') (tcxt, fcxt)
         res2 = checkFunc func2 (t2, t2') (tcxt, fcxt)
     _ -> Bad "invalid domain or codomain of tensor function.\n"
-  
+
   FTensUnit -> case (tyIn, tyOut) of
     (TTensor TUnit t, t') -> if t == t'
       then Ok ""
       else Bad "invalid domain or codomain of unit_*.\n"
     _ -> Bad "invalid domain or codomain of unit_*.\n"
-  
+
   FTensAssoc -> case (tyIn, tyOut) of
     (TTensor t1 (TTensor t2 t3), TTensor (TTensor t1' t2') t3') ->
       if t1 == t1' && t2 == t2' && t3 == t3'
       then Ok ""
       else Bad "invalid domain or codomain of assoc_*.\n"
     _ -> Bad "invalid domain or codomain of assoc_*.\n"
-  
+
   FTensSym -> case (tyIn, tyOut) of
     (TTensor t1 t2, TTensor t2' t1') ->
       if t1 == t1' && t2 == t2'
       then Ok ""
       else Bad "invalid domain or codomain of sym_*.\n"
     _ -> Bad "invalid domain or codomain of sym_*.\n"
-  
+
   FSum func1 func2 -> case (tyIn, tyOut) of
     (TSum t1 t2, TSum t1' t2') ->
       case (res1, res2) of
@@ -136,39 +136,39 @@ checkFunc func (tyIn, tyOut) (tcxt, fcxt) = case func of
         res1 = checkFunc func1 (t1, t1') (tcxt, fcxt)
         res2 = checkFunc func2 (t2, t2') (tcxt, fcxt)
     _ -> Bad "invalid domain or codomain of sum function.\n"
-  
+
   FSumAssoc -> case (tyIn, tyOut) of
     (TSum t1 (TSum t2 t3), TSum (TSum t1' t2') t3') ->
       if t1 == t1' && t2 == t2' && t3 == t3'
       then Ok ""
       else Bad "invalid domain or codomain of assoc_+.\n"
     _ -> Bad "invalid domain or codomain of assoc_+.\n"
-  
+
   FSumSym -> case (tyIn, tyOut) of
     (TSum t1 t2, TSum t2' t1') ->
       if t1 == t1' && t2 == t2'
       then Ok ""
       else Bad "invalid domain or codomain of sym_+.\n"
     _ -> Bad "invalid domain or codomain of sym_+.\n"
-  
+
   FDistrib -> case (tyIn, tyOut) of
     (TTensor (TSum t1 t2) t3, TSum (TTensor t1' t3') (TTensor t2' t3'')) ->
       if t1 == t1' && t2 == t2' && t3 == t3' && t3' == t3''
       then Ok ""
       else Bad "invalid domain or codomain of distrib.\n"
     _ -> Bad "invalid domain or codomain of distrib.\n"
-  
+
   FEval t -> case (tyIn, tyOut) of
     (TTensor t' (TStar t''), TUnit) ->
       if t == t' && t' == t''
       then Ok ""
       else Bad "invalid domain or codomain of eval.\n"
     _ -> Bad "invalid domain or codomain of eval.\n"
-  
+
   FDagger f -> checkFunc f (tyOut, tyIn) (tcxt, fcxt)
-  
+
   FVar (Ident str) ->
-    case (Map.lookup str fcxt) of
+    case Map.lookup str fcxt of
       Just _ -> Ok ""
       Nothing -> Bad $ "not found type variable " ++ str ++ ".\n"
 
@@ -179,58 +179,58 @@ searchOutType :: Func -> Type -> Context Iso -> Maybe Type
 searchOutType func inType cxt = case (func, inType) of
   (FId, t) ->
     Just t
-  
+
   (FComp f1 f2, t1) ->
     case maybe2 of
       Just t2 -> searchOutType f2 t2 cxt
       Nothing -> Nothing
     where
       maybe2 = searchOutType f1 t1 cxt
-  
+
   (FTensor f1 f2, TTensor t1 t2) ->
     case (maybe1, maybe2) of
       (Just t1', Just t2') -> Just (TTensor t1' t2')
       _ -> Nothing
     where
-      maybe1 = (searchOutType f1 t1 cxt)
-      maybe2 = (searchOutType f2 t2 cxt)
-  
+      maybe1 = searchOutType f1 t1 cxt
+      maybe2 = searchOutType f2 t2 cxt
+
   (FTensUnit, TTensor TUnit t) ->
     Just t
-  
+
   (FTensAssoc, TTensor t1 (TTensor t2 t3)) ->
     Just (TTensor (TTensor t1 t2) t3)
-  
+
   (FTensSym, TTensor t1 t2) ->
     Just (TTensor t2 t1)
-  
+
   (FSum f1 f2, TSum t1 t2) ->
     case (maybe1, maybe2) of
       (Just t1', Just t2') -> Just (TSum t1' t2')
       _ -> Nothing
     where
-      maybe1 = (searchOutType f1 t1 cxt)
-      maybe2 = (searchOutType f2 t2 cxt)
-  
+      maybe1 = searchOutType f1 t1 cxt
+      maybe2 = searchOutType f2 t2 cxt
+
   (FSumAssoc, TSum t1 (TSum t2 t3)) ->
     Just (TSum (TSum t1 t2) t3)
-  
+
   (FSumSym, TSum t1 t2) ->
     Just (TSum t2 t1)
-  
+
   (FDistrib, TTensor (TSum t1 t2) t3) ->
     Just (TSum (TTensor t1 t3) (TTensor t2 t3))
-  
+
   (FEval t, TTensor t' (TStar t'')) ->
     if t == t' && t' == t''
     then Just TUnit
     else Nothing
-  
+
   (FDagger f, t) ->
     searchInType f t cxt
-  
+
   (FVar (Ident str), t) ->
-    case (Map.lookup str cxt) of
+    case Map.lookup str cxt of
       Just (_, tyIn, tyOut) ->
         if t == tyIn
         then Just tyOut
@@ -239,49 +239,49 @@ searchOutType func inType cxt = case (func, inType) of
 
   (FShift _, t) ->
     Just t
-  
+
   _ -> Nothing
 
 searchInType :: Func -> Type -> Context Iso -> Maybe Type
 searchInType func outType cxt = case (func, outType) of
   (FId, t) ->
     Just t
-  
+
   (FComp f1 f2, t3) ->
     case maybe2 of
       Just t2 -> searchInType f1 t2 cxt
       Nothing -> Nothing
     where
       maybe2 = searchInType f2 t3 cxt
-  
+
   (FTensor f1 f2, TTensor t1 t2) ->
     case (maybe1, maybe2) of
       (Just t1', Just t2') -> Just (TTensor t1' t2')
       _ -> Nothing
     where
-      maybe1 = (searchInType f1 t1 cxt)
-      maybe2 = (searchInType f2 t2 cxt)
-  
+      maybe1 = searchInType f1 t1 cxt
+      maybe2 = searchInType f2 t2 cxt
+
   (FTensUnit, t) ->
     Just (TTensor TUnit t)
-  
+
   (FTensAssoc, TTensor (TTensor t1 t2) t3) ->
     Just (TTensor t1 (TTensor t2 t3))
-    
+
   (FTensSym, TTensor t1 t2) ->
     Just (TTensor t2 t1)
-  
+
   (FSum f1 f2, TSum t1 t2) ->
     case (maybe1, maybe2) of
       (Just t1', Just t2') -> Just (TSum t1' t2')
       _ -> Nothing
     where
-      maybe1 = (searchInType f1 t1 cxt)
-      maybe2 = (searchInType f2 t2 cxt)
+      maybe1 = searchInType f1 t1 cxt
+      maybe2 = searchInType f2 t2 cxt
 
   (FSumAssoc, TSum (TSum t1 t2) t3) ->
     Just (TSum t1 (TSum t2 t3))
-  
+
   (FSumSym, TSum t1 t2) ->
     Just (TSum t2 t1)
 
@@ -292,12 +292,12 @@ searchInType func outType cxt = case (func, outType) of
 
   (FEval t, TUnit) ->
     Just (TTensor t (TStar t))
-  
+
   (FDagger f, t) ->
     searchOutType f t cxt
-  
+
   (FVar (Ident str), t) ->
-    case (Map.lookup str cxt) of
+    case Map.lookup str cxt of
       Just (_, tyIn, tyOut) ->
         if t == tyOut
         then Just tyIn
@@ -306,5 +306,5 @@ searchInType func outType cxt = case (func, outType) of
 
   (FShift _, t) ->
     Just t
-  
+
   _ -> Nothing
